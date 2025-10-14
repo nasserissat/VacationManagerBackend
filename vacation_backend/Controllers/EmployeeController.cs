8e2812416576ddc53a47eea5f3ca3a05c1fb8736
nasser25 <nasseremil25@gmail.com>
2025-10-13 20:30:19 -0400

    Employee Controller implemented

---
+++ vacation_backend/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using vacation_backend.Application.DTOs.Employee;
+using vacation_backend.Application.Interfases.IServices;
+using vacation_backend.Application.Services;
+
+namespace vacation_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees([FromQuery] EmployeeFilterDto filters)
+        {
+            var result = await _employeeService.GetAllEmployeesAsync(filters);
+            return Ok(result);
+        }
+    }
+}
