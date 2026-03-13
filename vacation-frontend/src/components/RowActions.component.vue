294f72f1bacce03de4eec9ff093fb6b49821e54b
nasser25 <nasseremil25@gmail.com>
2026-03-13 19:55:37 -0400

    Added RowActions component

---
+++ vacation-frontend/src/components/RowActions.component.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row-actions">
+    <button
+      class="btn btn-gray btn-sm"
+      @click="$emit('edit')"
+      :disabled="disabled"
+    >
+      <fa-icon icon="pen-to-square" />
+    </button>
+
+    <button
+      class="btn btn-danger btn-sm"
+      @click="$emit('cancel')"
+      :disabled="disabled"
+    >
+      <fa-icon icon="ban" />
+    </button>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  disabled: {
+    type: Boolean,
+    default: false
+  }
+})
+
+defineEmits(['edit', 'cancel'])
+</script>
